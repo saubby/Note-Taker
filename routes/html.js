ebc9736e011418b0163730a951be2c9ede471a96
saubby <saubby27@gmail.com>
2023-01-29 15:17:06 -0500

    html router

---
+++ routes/html.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const express = require("express");
+const router = express.Router();
+const app = express();
+
+
+
+router.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+
+router.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = router;
